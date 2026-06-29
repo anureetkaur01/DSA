616e40e50a3d41265870d62fcf6635c5a3822663
anureetkaur01 <kanureet001@gmail.com>
2026-06-29 22:00:22 +0530

    Update BST implementation

--- Trees/implement-bst.cpp
+++ Trees/implement-bst.cpp
@@ -32,17 +32,37 @@ return new BSTNODE(val);
         InOrderTraversal(root->right);
 
     }
+    void PreOrderTraversal(BSTNODE *root){
+        if(root==NULL)  return;
+        cout<<root->info<<" ";
+        PreOrderTraversal(root->left);
+        PreOrderTraversal(root->right);
+
+    }
+    void PostOrderTraversal(BSTNODE *root){
+        if(root==NULL)
+        return;
+
+        PostOrderTraversal(root->left);
+        PostOrderTraversal(root->right);
+        cout<<root->info<<" ";
+
+    }
 };
 int main(){
     TREE obj;
     BSTNODE *root=NULL;
     root=obj.append(root,100);
     root=obj.append(root,90);
-    root=obj.append(root,110);
+   // root=obj.append(root,110);
     root=obj.append(root,80);
-        root=obj.append(root,95);
+        root=obj.append(root,70);
     root=obj.append(root,200);
     obj.InOrderTraversal(root);
+    cout<<endl;
+    obj.PreOrderTraversal(root);
+    cout<<endl;
+    obj.PostOrderTraversal(root);
 return 0;
     
 }
