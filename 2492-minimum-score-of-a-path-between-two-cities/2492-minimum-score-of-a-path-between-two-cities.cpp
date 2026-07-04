856e51c25fdb0c894b787b1858211ef478530a22
anureetkaur01 <kanureet001@gmail.com>
2026-07-04 08:55:55 +0530

    Time: 295 ms (5.01%), Space: 134.2 MB (56.91%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<pair<int,int>>> &adj,
-             vector<int> &vis, int &ans) {
-
-        vis[node] = 1;
-
-        for (auto &it : adj[node]) {
-            int nei = it.first;
-            int wt = it.second;
-
-            ans = min(ans, wt);
-
-            if (!vis[nei]) {
-                dfs(nei, adj, vis, ans);
-            }
-        }
-    }
-
     int minScore(int n, vector<vector<int>>& roads) {
 
         vector<vector<pair<int,int>>> adj(n + 1);
@@ -27,9 +10,25 @@ public:
         }
 
         vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+
         int ans = INT_MAX;
 
-        dfs(1, adj, vis, ans);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &it : adj[node]) {
+                ans = min(ans, it.second);
+
+                if (!vis[it.first]) {
+                    vis[it.first] = 1;
+                    q.push(it.first);
+                }
+            }
+        }
 
         return ans;
     }
