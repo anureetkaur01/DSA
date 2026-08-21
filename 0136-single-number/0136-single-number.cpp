6a37760c2391147757418261b1d7650be767eea5
anureetkaur01 <kanureet001@gmail.com>
2026-08-21 16:18:44 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.07%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(auto v:nums){
+            ans=ans^v;
+        }
+        return ans;
+    }
+};
